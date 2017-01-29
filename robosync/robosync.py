bad8e080a6a61863565148ac74db77babdbcae7d
rbn920 <robby@nelson-wilcox.com>
2017-01-29 15:49:49 -0800

    Mirror  using robocopy

---
+++ robosync/robosync.py
@@ -0,0 +1,19 @@
+import subprocess
+import sys
+
+def read(txtfile):
+    with open(txtfile, r) as f:
+        dir_names = [folder for folder in f]
+
+    return dir_names
+
+main(infile, outfile):
+
+    sources = read(infile)
+    destinations = read(outfile)
+    for s, d in zip(sources, destinations):
+        subprocess.call(["robocopy", s, d, "/MIR"])
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
+
