56a00a89f86fd91ce7aeab9abae03016479e3070
rbn920 <robby@nelson-wilcox.com>
2017-01-29 16:13:40 -0800

    Change to single quotes

--- robosync/robosync.py
+++ robosync/robosync.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 def read(txtfile):
-    with open(txtfile, r) as f:
+    with open(txtfile, 'r') as f:
         dir_names = [folder for folder in f]
 
     return dir_names
@@ -12,8 +12,8 @@ main(infile, outfile):
     sources = read(infile)
     destinations = read(outfile)
     for s, d in zip(sources, destinations):
-        subprocess.call(["robocopy", s, d, "/MIR"])
+        subprocess.call(['robocopy', s, d, '/MIR'])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
 
