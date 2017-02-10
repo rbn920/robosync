07b21e9230f17a937c777eebe95e47c7d4fb0bff
rbn920 <robby@nelson-wilcox.com>
2017-02-09 21:11:47 -0800

    Initial test file

--- robosync/test/test_robosync.py
+++ robosync/test/test_robosync.py
@@ -4,12 +4,14 @@ import os
 class testMirror(unittest.TestCase):
 
     def setUp(self):
-        dirnames = ['dir1', 'dir2', 'dir3']
+        source_dirs = ['dir1', 'dir2', 'dir3']
         filenames = ['file1.txt', 'file2.txt', 'file3.txt']
         for d_name, f_name in zip(dirnames, filenames):
             os.mkdir(d_name)
             with open(os.path.join(d_name, f_name), 'w') as f:
                 f.write('foobar')
+
+    def test_mirror
 
 if __name__ == '__main__':
     unittest.main()
