c5b924fd47520c3920cd146ea85bc98da04bd6af
rbn920 <robby@nelson-wilcox.com>
2017-02-11 13:36:46 -0800

    Add __pycache__ to ignore

--- robosync/test/test_robosync.py
+++ robosync/test/test_robosync.py
@@ -1,24 +1,24 @@
 import unittest
 import os
 import shutil
-from robosync import *
+import robosync
 
 class testMirror(unittest.TestCase):
 
     def setUp(self):
         os.mkdir('test_source')
         os.mkdir('test_dest')
-        source_dirs = ['dir1', 'dir2', 'dir3']
-        dest_dirs = ['dir1_c', 'dir2_c', 'dir3_c']
+        self.source_dirs = ['dir1', 'dir2', 'dir3']
+        self.dest_dirs = ['dir1_c', 'dir2_c', 'dir3_c']
         filenames = ['file1.txt', 'file2.txt', 'file3.txt']
         contents = ['foobar1', 'foobar2', 'foobar3']
         with open('source_file.txt', 'w') as f:
-            f.write('\n'.join(source_dirs))
+            f.write('\n'.join(self.source_dirs))
 
         with open('dest_file.txt', 'w') as f:
-            f.write('\n'.join(dest_dirs))
+            f.write('\n'.join(self.dest_dirs))
 
-        for d_name, f_name, content, in zip(source_dirs, filenames, contents):
+        for d_name, f_name, content, in zip(self.source_dirs, filenames, contents):
             new_dir = os.path.join('test_source', d_name)
             os.mkdir(new_dir)
             with open(os.path.join('test_source', d_name, f_name), 'w') as f:
@@ -31,6 +31,12 @@ class testMirror(unittest.TestCase):
         os.remove('source_file.txt')
         os.remove('dest_file.txt')
 
+    def test_read(self):
+        inlist = robosync.read('source_file.txt')
+        outlist = robosync.read('dest_file.txt')
+        self.assertListEqual(inlist, self.source_dirs)
+        self.assertListEqual(outlist, self.dest_dirs)
+        self.assertListEqual(inlist, [1, 2, 3])
 
     def test_mirror(self):
         pass
