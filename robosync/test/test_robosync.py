c4632362074093a6befe7dad966514268b348c20
rbn920 <robby@nelson-wilcox.com>
2017-02-11 12:34:28 -0800

    Add setup and teardown

--- robosync/test/test_robosync.py
+++ robosync/test/test_robosync.py
@@ -1,17 +1,29 @@
 import unittest
 import os
+import shutil
 
 class testMirror(unittest.TestCase):
 
     def setUp(self):
+        os.mkdir('test_source')
+        os.mkdir('test_dest')
         source_dirs = ['dir1', 'dir2', 'dir3']
         filenames = ['file1.txt', 'file2.txt', 'file3.txt']
-        for d_name, f_name in zip(dirnames, filenames):
-            os.mkdir(d_name)
-            with open(os.path.join(d_name, f_name), 'w') as f:
-                f.write('foobar')
+        contents = ['foobar1', 'foobar2', 'foobar3']
+        for d_name, f_name, content, in zip(source_dirs, filenames, contents):
+            new_dir = os.path.join('test_source', d_name)
+            os.mkdir(new_dir)
+            with open(os.path.join('test_source', d_name, f_name), 'w') as f:
+                f.write(content)
 
-    def test_mirror
+
+    def tearDown(self):
+        shutil.rmtree('test_source')
+        shutil.rmtree('test_dest')
+
+
+    def test_mirror(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
